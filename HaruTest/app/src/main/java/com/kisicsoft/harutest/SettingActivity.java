07ce841964b655fc951cd0e3f3be0d4263830a3a
Kisic Kim <50286174+kimkisic@users.noreply.github.com>
2019-11-24 21:20:10 +0900

    update
    SettingActivity

--- HaruTest/app/src/main/java/com/kisicsoft/harutest/SettingActivity.java
+++ HaruTest/app/src/main/java/com/kisicsoft/harutest/SettingActivity.java
@@ -1,17 +1,14 @@
 package com.kisicsoft.harutest;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.preference.PreferenceActivity;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.Preference;
+
+import android.os.Bundle;
 
 public class SettingActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction().replace(R.id.settingcontainer, new MySettingFragment()).commit();
+        setContentView(R.layout.activity_setting);
     }
 }
